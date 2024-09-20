ca4f658aa14cbb5518ac981803045ca6bc7e61b7
markfialko <markfialkofialko@gmail.com>
2024-09-20 22:04:40 +0500

    chore: update package.json

--- backend/dist/utils/utils.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.stripUndefined = stripUndefined;
-function stripUndefined(obj) {
-    return JSON.parse(JSON.stringify(obj));
-}
